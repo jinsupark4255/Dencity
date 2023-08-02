9d9386a7cd0f008d3ee7f3c07b131f6eef2f1d7a
jinsupark4255 <jin04007@naver.com>
2023-08-02 23:25:37 +0900

    카카오 로그인 기능 및 데이터 받아오는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './contents/mainpage';
+import KakaoLogin from './contents/kakaoLogin';
+import Profile from './contents/profile';
+import { UserProvider } from './contents/UserContext';
 
 function App() {
   return (
-    <Router>
+    <UserProvider>
+<Router>
       <Routes>
+        <Route path="/" element={ <KakaoLogin/>} />
+        <Route path="/profile" element={<Profile/>}/>
         <Route path="/main" element={<MainPage />} />
       </Routes>
     </Router>
+    </UserProvider>
+    
   );
 }
 
